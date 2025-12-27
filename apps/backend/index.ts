edc50bbbac8cf38e3cae86cd5e333dd24d15358c
Aditya Kumar Singh <singhadityakumar1711@gmail.com>
2025-12-28 02:38:10 +0530

    mongoose types setup

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,9 +1,41 @@
 import express from "express";
+import mongoose from "mongoose";
+import { UserModel } from "db/client";
+import {SignupSchema} from "common/types"
+import "dotenv/config";
+
+const uri = process.env.MONGO_URL;
+if (!uri) throw new Error("MONGO_URI is not defined");
 
 const app = express();
+await mongoose.connect(uri)
+console.log("✅ Connected to MongoDB Atlas");
+app.use(express.json());
 
-app.post("/signup", (req, res)=>{
-
+app.post("/signup", async (req, res)=>{
+    const {success, data} = SignupSchema.safeParse(req.body);
+    if (!success){
+        res.status(403).json({
+            message: "Incorrect Inputs"
+        })
+        return 
+    }
+    try{
+        const user = await UserModel.create({
+            username: data.username,
+            password: data.password
+        })
+        res.json({
+            id: user._id
+        })
+    }
+    catch(e){
+        res.status(411).json({
+            message: "Username already exists",
+            error: e
+        })
+    }
+    
 })
 
 app.post("/signin", (req,res)=>{
---
+++ packages/db/index.ts
@@ -0,0 +1,131 @@
+import { password } from "bun";
+import mongoose, {Schema} from "mongoose";
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+});
+
+const EdgesSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    source: {
+        type:String,
+        required:true
+    },
+    target:{
+        type:String,
+        required:true
+    }
+}, {_id:false})
+
+const PositionSchema = new Schema({
+    x:{
+        type:Number,
+        required:true
+    },
+    y:{
+        type:Number,
+        required:true
+    }
+}, {_id:false})
+
+const NodeDataSchema = new Schema({
+    kind:{
+        type:String,
+        enum: ['ACTION', 'TRIGGER'],
+        required:true
+    },
+    metadata:{
+        type:Schema.Types.Mixed,
+        required:true
+    }
+}, {_id:false})
+
+const WorkflowNodesSchema = new Schema({
+    nodeId:{
+        type:mongoose.Types.ObjectId,
+        required:true,
+        ref: "Nodes"
+    },
+    data: NodeDataSchema,
+    id: {
+        type:String,
+        required:true
+    },
+    position: PositionSchema,
+    credentials: Schema.Types.Mixed
+}, {_id:false})
+
+const WorkflowSchema = new Schema({
+    userId:{
+        type: mongoose.Types.ObjectId,
+        required:true,
+        ref: 'Users'
+    },
+    nodes: [WorkflowNodesSchema],
+    edges:[EdgesSchema]
+});
+
+const CredentialsTypeSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    required:{
+        type: Boolean,
+        required:true
+    }
+}, {_id:false})
+
+const NodesSchema = new Schema({
+    title:{
+        type:String,
+        required:true   
+    },
+    description:{
+        type: String,
+        required:true
+    },
+    type:{
+        type: String,
+        enum: ['ACTION', 'TRIGGER'],
+        required:true
+    },
+    credentialsType: [CredentialsTypeSchema]
+});
+
+const ExecutionsSchema = new Schema({
+    workflowId:{
+        type: mongoose.Types.ObjectId,
+        required:true,
+        ref:'Workflows'
+    },
+    status:{
+        type:String,
+        required:true
+    },
+    startTime:{
+        type:Date,
+        default:Date.now()
+    },
+    endTime:{
+        type:Date
+    }
+})
+
+
+
+export const UserModel = mongoose.model("Users", UserSchema);
+export const WorkflowModel = mongoose.model("Workflows", WorkflowSchema);
+export const NodesModel = mongoose.model("Nodes", NodesSchema);
+export const ExecutionsModel = mongoose.model("Execuutions", ExecutionsSchema);
